2a754b08faffd11d3947d0afa0ed679459d080d0
manas <manas@wonderbox.(none)>
2013-10-13 18:15:08 -0700

    frame

--- socketio_namespaces.py
+++ socketio_namespaces.py
@@ -6,39 +6,6 @@ import time
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-class PlayersNamespace(BaseNamespace):
-  def recv_connect(self):
-    print "Got a socket connection" # debug
-    self.ps = r.pubsub()
-  
-  def on_datacall(self,packet):
-    r.incr('ctr')
-    #print("in on sync and packet args: "+str(packet['args']));
-    #r.set(self.id,json.dumps(packet['args'][0]['data']))
-    #if self.partner_id is not None and self.partner_id != -1:
-    #  self.emit('sync',{'partnerData':json.loads(r.get(self.partner_id))})
-    print("don test")
-  
-  def on_login(self, packet):
-    print('login called');
-    self.id = r.get('sessionid')
-    r.incr('sessionid')
-    if packet['args'][0]['multi']:
-      r.lpush('players',self.id)
-      self.ps.subscribe([self.id])
-      for item in self.ps.listen():
-        print 'got an item from listen'+str(item)
-        if(item['type'] == 'message'):
-          data = json.loads(item['data'])
-          self.partner_id = int(data['partner'])
-          self.emit('login',{'id':self.id,'partner': data['partner'],'pieces':data['pieces']})
-    else:
-      self.emit('login',{'id':self.id,'partner': -1,'pieces':[]})
-      
-  def disconnect(self, *args, **kwargs):
-    print "Got a socket disconnection" # debug
-    self.ps.unsubscribe()
-    if self.id is not None:
-        r.delete(self.id)
-    super(PlayersNamespace, self).disconnect(*args, **kwargs)
-
+class ChatNamespace(BaseNamespace):
+    def on_chat(self, msg):
+        self.emit('chat', msg)
