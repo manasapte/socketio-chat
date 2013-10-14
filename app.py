3ae9ebeb0a53de20bbc73353e61484d82b33dedf
manas <manas@wonderbox.(none)>
2013-10-13 17:47:38 -0700

    framework more

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 from flask import Flask, render_template, request, session, abort, jsonify
-from socketio_namespaces import PlayersNamespace 
+from socketio_namespaces import StockNamespace
 from socketio import socketio_manage
 import json
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/tetris')
-def index():
-    return render_template('tetris.html') 
-
 @app.route('/socket.io/<path:rest>')
 def push_stream(rest):
     try:
-        socketio_manage(request.environ, {'/players' : PlayersNamespace}, request)
+        socketio_manage(request.environ, {'/stock' : StockNameSpace}, request)
     except:
         app.logger.error("Exception while handling socketio connection",
                      exc_info=True)
